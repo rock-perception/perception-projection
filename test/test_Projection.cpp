988ba4e10151d960513a1ea810484ca4dcc4fd41
Jakob Schwendner <jakob.schwendner@dfki.de>
2013-10-28 11:12:35 +0100

    fixed test for generating homography matrix

--- test/test_Projection.cpp
+++ test/test_Projection.cpp
@@ -11,17 +11,39 @@
 using namespace std;
 //using namespace projection;
 
-Eigen::Matrix3f calcHomography( const Eigen::Matrix3f &R, const Eigen::Vector3f& t, float d )
+/**
+ * Comes from:
+ * http://hal.inria.fr/docs/00/17/47/39/PDF/RR-6303.pdf
+ * Deeper understanding of the homography decomposition for vision-based control
+ */
+Eigen::Matrix3f calcHomography( const Eigen::Matrix3f &R, const Eigen::Vector3f& t, const Eigen::Vector3f& n, float d )
 {
-    Eigen::Vector3f p = R.transpose() * Eigen::Vector3f::UnitZ() / d;
-    float k = pow( (1.0+p.transpose()*t), 1.0/3.0 );
-    Eigen::Matrix3f A = 1.0/k * (Eigen::Matrix3f::Identity() + p*t.transpose()) * R;
+    Eigen::Matrix3f A = R + (t*n.transpose()) / d;
     return A;
 }
 
-Eigen::Matrix3f calcHomography( const Eigen::Isometry3f &trans, float dist )
+Eigen::Matrix3f calcHomography( const Eigen::Isometry3f &trans, const Eigen::Vector3f &n, float dist )
 {
-    return calcHomography( trans.rotation(), trans.translation(), dist );
+    return calcHomography( trans.rotation(), trans.translation(), n, dist );
+}
+
+/**
+ * camA - current frame
+ * camB - desired frame
+ */
+Eigen::Matrix3f calcHomography( const Eigen::Isometry3f &camA2plane, const Eigen::Isometry3f &camB2plane  )
+{
+    // calculate the plane normal (z-vector) from the point of camB
+    Eigen::Vector3f n = camB2plane.rotation().transpose() * -Eigen::Vector3f::UnitZ();
+
+    // calculate the distance from camB to plane
+    float d = camB2plane.inverse().translation().dot( n );
+
+    // get transform from camB to camA
+    Eigen::Isometry3f camBtoCamA = camA2plane.inverse() * camB2plane;
+
+    // get the homography
+    return calcHomography( camBtoCamA, n, d );
 }
 
 Eigen::Matrix3f getCameraMatrix( float fx, float fy, float cx, float cy )
@@ -43,19 +65,24 @@ BOOST_AUTO_TEST_CASE( image_projection )
 
     // calculate Homography matrix
     cv::Mat hom_cv( 3, 3, CV_32FC1 );
-    Eigen::Isometry3f trans = Eigen::Isometry3f::Identity();
-    //trans.rotate( Eigen::AngleAxisf( M_PI/8.0, Eigen::Vector3f::UnitX() ) );
-    trans.translate( Eigen::Vector3f( 0, 0, 3 ) );
-    trans.rotate( Eigen::AngleAxisf( M_PI/2.0, Eigen::Vector3f::UnitX() ) );
 
-    std::cout << trans.matrix() << std::endl;
+    Eigen::Isometry3f camA2plane = 
+        Eigen::Isometry3f( Eigen::Translation3f( Eigen::Vector3f( 0, 0, -1 ) ) ) *
+        Eigen::AngleAxisf( M_PI*0.435, Eigen::Vector3f::UnitX() ) * 
+        Eigen::AngleAxisf( M_PI, Eigen::Vector3f::UnitX() ) ;
 
-    Eigen::Matrix3f cam1 = getCameraMatrix( 200, 200, src.cols / 2, src.rows / 2 );
-    Eigen::Matrix3f hom = cam1 * calcHomography( trans, 1.0 ) * cam1.inverse();
+    Eigen::Isometry3f camB2plane = 
+        Eigen::Isometry3f( Eigen::Translation3f( Eigen::Vector3f( 0, 0, -10 ) ) ) *
+        Eigen::AngleAxisf( M_PI, Eigen::Vector3f::UnitX() );
+
+    Eigen::Matrix3f cam1 = getCameraMatrix( 350, 350, src.cols / 2, src.rows / 2 );
+    Eigen::Matrix3f H = calcHomography( camA2plane, camB2plane ); 
+
+    Eigen::Matrix3f hom = cam1 * H.inverse() * cam1.inverse();
     eigen2cv( hom, hom_cv );
 
     // apply perspective Transform
-    cv::warpPerspective( src, dst, hom_cv, dst.size() ); 
+    cv::warpPerspective( src, dst, hom_cv, dst.size(), cv::INTER_LINEAR  ); 
     
     // show output
     cv::imshow( "projection", dst );
