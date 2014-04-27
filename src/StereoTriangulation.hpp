d93b0a2b70fae143b4670b729e6233456c9dd73c
Jakob Schwendner <jakob.schwendner@gmail.com>
2014-04-27 10:07:28 +0200

    added method to calculate the direction of the scenepoint based on angles of the camera points

--- src/StereoTriangulation.hpp
+++ src/StereoTriangulation.hpp
@@ -3,6 +3,7 @@
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <cmath>
 
 namespace projection
 {
@@ -11,6 +12,11 @@ struct StereoTriangulation
     Eigen::Isometry3d trans;
     Eigen::Vector3d scenePoint;
     double error;
+
+    Eigen::Vector3d direction;
+
+    Eigen::Vector3d xl, xr;
+    Eigen::Vector3d vl, vr;
 
     StereoTriangulation()
         : trans( Eigen::Isometry3d::Identity() ),
@@ -26,7 +32,6 @@ struct StereoTriangulation
     void calcScenePoint( const Eigen::Vector2d& p1, const Eigen::Vector2d& p2 )
     {
         // get homogenous coordinates
-        Eigen::Vector3d xl, xr;
         xl << p1, 1;
         xr << p2, 1; 
 
@@ -48,9 +53,30 @@ struct StereoTriangulation
         error = 0.5 * Vp.norm();
     }
 
+    void calcDirection( double a1, double a2 )
+    {
+        // calc angle vectors
+        vl << std::cos(a1), std::sin(a1), 0;
+        vr << std::cos(a2), std::sin(a2), 0;
+        
+        Eigen::Quaterniond R(trans.rotation());
+        Eigen::Matrix<double,3,3> A;
+        A << xl, vl, -R.inverse()*(xr+vr);
+        Eigen::Vector3d b = Eigen::Vector::Zeros();
+        Eigen::Vector3d param = A.colPivHouseholderQr().solve(b);
+        
+        Eigen::Vector3d v = param[0] * xl + param[1] * vl;
+        direction = v.normalized();
+    }
+
     Eigen::Vector3d getScenePoint() const
     {
         return scenePoint;
+    }
+
+    Eigen::Vector3d getDirection() const
+    {
+        return direction;
     }
 
     double getError() const
